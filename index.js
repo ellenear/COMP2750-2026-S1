4fec3210e7c1b7f758e0b303d3c38bc6e532ff7e
julia437 <julia.abraham@students.mq.edu.au>
2026-05-24 11:20:01 +1000

    Home page

---
+++ index.js
@@ -0,0 +1,34 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js";
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDsmzCtW5ADthBJv_PNdz_trDb8BSn8WL0",
+    authDomain: "campus-marketplace-70b95.firebaseapp.com",
+    projectId: "campus-marketplace-70b95",
+    storageBucket: "campus-marketplace-70b95.firebasestorage.app",
+    messagingSenderId: "1017814633308",
+    appId: "1:1017814633308:web:25463e96717e7e024fcf99"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+onAuthStateChanged(auth, function(user) {
+    if (user) {
+        document.getElementById("emailInput").innerText = user.email;
+    } else {
+        window.location.href = "login.html";
+    }
+});
+
+const signOutBtn = document.getElementById("signOutBtn"); 
+if (signOutBtn) {
+    signOutBtn.addEventListener("click", async function() {
+        try {
+            await signOut(auth);
+            window.location.href = "login.html";
+        } catch (error) {
+            console.error("Error signing out:", error);
+        }
+    });
+}
